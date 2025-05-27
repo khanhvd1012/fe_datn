4758da2e94916654f9444be52488ee14a1cea299
khanhvd1012 <khanhvdph49723@gmail.com>
2025-05-27 15:47:50 +0700

    sua lai gdien

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,6 @@ import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Routes>
