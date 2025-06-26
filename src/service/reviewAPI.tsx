c7d7101a2bd9efbdf3a647283b6864e10c9695e1
khanhvd1012 <khanhvdph49723@gmail.com>
2025-06-26 13:49:23 +0700

    update review

--- src/service/reviewAPI.tsx
+++ src/service/reviewAPI.tsx
@@ -1,20 +1,11 @@
-
+// src/services/reviewApi.ts
 import axios from "axios";
 import type { IReview } from "../interface/review";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-export const getReviewsByProduct = async (product_id: string): Promise<IReview[]> => {
-  const response = await axios.get(`${API_URL}/reviews/${product_id}`);
-  return response.data; // 👈 Đảm bảo backend trả mảng []
-};
-
-export const getReviewById = async (reviewId: string): Promise<IReview> => {
-    try {
-        const response = await axios.get(`${API_URL}/reviews/${reviewId}`);
-        return response.data;
-    } catch (error) {
-        console.error("Error fetching review:", error);
-        throw error;
-    }
+// ✅ Gọi API lấy tất cả đánh giá
+export const getAllReviews = async (): Promise<IReview[]> => {
+  const response = await axios.get(`${API_URL}/reviews`);
+  return response.data.reviews; // Vì backend trả về { success, reviews }
 };
